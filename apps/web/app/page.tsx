49ea83e02cf76e9454775e4bcc247ef1df0d37b1
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2024-04-10 15:31:33 -0300

    aaaaa

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -79,7 +79,7 @@ export default function Page(): JSX.Element {
       </div>
 
       <Button appName="web" className={styles.button}>
-        Aobaaaa
+        Reuniao
       </Button>
 
       <div className={styles.hero}>
